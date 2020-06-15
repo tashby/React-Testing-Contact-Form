df6b370ed5308e714970b3c21e09192971f109fe
Tashby <tashby2714@gmail.com>
2020-06-15 18:04:33 -0400

    tests up

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText } from "@testing-library/react";
 import App from "./App";
+
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("first and last name fields", () => {
+   const { getByText } = render(<App />);
+   getByText(/first name/i);
+   getByText(/last name/i);
+})
+
+test("dummy text", () => {
+   const { getByPlaceholderText } = render(<App />);
+   getByPlaceholderText('Edd');
+})
